991a18b5f885ba14e0661f149730c6bb6043f874
Carl Hinderer <carl.hinderer4@gmail.com>
2022-06-21 14:29:31 -0600

    Added flask-marshmallow and marshmallow-sqlalchemy packages.

--- api/routes/hotel.py
+++ api/routes/hotel.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, jsonify, request
 from http import HTTPStatus
 
-# from models import Hotel
+from models import Hotel
 
 hotel = Blueprint(name='hotel', import_name=__name__)
 
 
 @hotel.route('/hotels', methods=['GET'])
 def get_hotels():
-    return jsonify({'data': 'Data Here!'})
+    hotel_name = Hotel.query.first().name
+    return jsonify({'data': hotel_name})
