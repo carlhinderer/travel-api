e47b7b439b5fd77bc42d731c66e90139e8e29ee8
Carl Hinderer <carl.hinderer4@gmail.com>
2022-06-29 23:09:20 -0600

    Fixed configuration issues.

--- app/config.py
+++ app/config.py
@@ -7,9 +7,3 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://travelapiuser:travelapipw@localhost/trav
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-placeholder'
-
-
-# Test Config
-# TESTING = True
-# SQLALCHEMY_DATABASE_URI = 'postgresql://travelapitestuser:travelapitestpw@localhost/travelapitest'
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
